77191a2e46c1229947b3fe6452f70b9feed418fa
kepengqi <kepengqi@gmail.com>
2025-04-13 15:09:46 +0800

    update: add new background colors and enhance course styles

--- src/components/course/index.tsx
+++ src/components/course/index.tsx
@@ -1,13 +1,12 @@
+import React from 'react';
+import { Box, Container } from '@mui/material';
 
-import React from "react";
-import { Box,Container } from "@mui/material";
-
-import CourseList from "./list";
+import CourseList from './list';
 
 export default function Home() {
   return (
-    <Box className="w-full mt-20">
-      <Container maxWidth="lg">
+    <Box className='w-full pt-20 bg-black'>
+      <Container maxWidth='lg'>
         <CourseList />
       </Container>
     </Box>
--- src/components/course/item.tsx
+++ src/components/course/item.tsx
@@ -1,55 +1,34 @@
-import React from "react";
+import React from 'react';
 
-import './style.css'
+import './style.css';
 
 interface CourseItemProps {}
-const CourseItem:React.FC<CourseItemProps> = ({
-}) => {
+const CourseItem: React.FC<CourseItemProps> = ({}) => {
   return (
-    <div className="col-lg-4 col-md-6">
-      <div className="course-shape-item hover-shape-border">
-        <div className="course-item">
-          <div className="course-info">
-            <a href="course-details.html">
-              <img src="/static/course/course-1.png" alt="Explore-Image" />
-            </a>
-            <div className="course-author">
-                <h2 className="pb-3 pt-3">
-                  <a href="project-details.html" className="text-2xl">
-                    Cyber City Clan
-                  </a>
-                </h2>
-              <p className="pb-3">描述信息描述信息描述信息描述信息描述信息描述信息描述信息</p>
-            </div>
-          </div>
-          <div className="course-content">
-            <div className="mb-1">
-              <ul className="course-listing">
-                <li className="pb-1 flex justify-between">
-                  <span>type</span>
-                  <span>xx</span>
-                </li>
-                <li className="pb-1 flex justify-between">
-                  <span>price</span>
-                  <span>900.00 BUSD</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <span className="border-shadow shadow-1"></span>
-          <span className="border-shadow shadow-2"></span>
-          <span className="border-shadow shadow-3"></span>
-          <span className="border-shadow shadow-4"></span>
-        </div>
-        <div className="actions-btn flex justify-between">
-          <button className="cursor-pointer font-sm rounded-md">Like</button>  
-          <button className="border px-4 py-1 bg-primary cursor-pointer font-sm rounded-md hover:bg-[#B5FF6D] hover:border-[#B5FF6D] hover:text-black">Buy</button>  
+    <div className='course-item'>
+      <div className='course-info'>
+        <img src='/static/course/course-1.png' alt='Course Image' />
+        <div className='course-author'>
+          <h2>Discover Endless Possibilities</h2>
+          <p>Where Creativity Meets Innovation. Embark on a Journey of Limitless Exploration.</p>
         </div>
       </div>
-      <span className="border-shadow shadow-1"></span>
-      <span className="border-shadow shadow-2"></span>
-      <span className="border-shadow shadow-3"></span>
-      <span className="border-shadow shadow-4"></span>
+      <div className='course-content'>
+        <ul className='course-listing'>
+          <li className='flex justify-between'>
+            <span>Category</span>
+            <span>Digital Art</span>
+          </li>
+          <li className='flex justify-between'>
+            <span>Price</span>
+            <span>900.00 BUSD</span>
+          </li>
+        </ul>
+      </div>
+      <div className='actions-btn'>
+        <button>Like</button>
+        <button>Buy Now</button>
+      </div>
     </div>
   );
 };
