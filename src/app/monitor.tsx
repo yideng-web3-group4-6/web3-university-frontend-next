8f02034154b133a249981b297f885e32dea7d548
anguske <anguske@aaxischina.com>
2025-04-17 15:28:33 +0800

    refactor: lazy load MonitorSdk in MonitorComponent

--- src/app/monitor.tsx
+++ src/app/monitor.tsx
@@ -1,29 +1,33 @@
 'use client';
-import React, { useEffect } from 'react';
-import MonitorSdk from './../../fe-monitor-sdk/lib/index.esm';
+import { useEffect } from 'react';
 
 const MonitorComponent = () => {
-    const monitorSdk = MonitorSdk({
-      pid: '1000',
-      reportUrl: 'http://127.0.0.1:9001/report',
-      debug: false,
-      jsErrorLog: true,
-      promiseErrorLog: true,
-      resourcesErrorLog: true,
-      exposureLog: true,
-      automaticBurialPointLog: true,
-      pageDwellTimeLog: true,
-      pvLog: true,
-      xhrLog: true,
-      resourcesPerfLog: true,
-      perfLog: true,
-    });
+    useEffect(() => {
+        import('./../../fe-monitor-sdk/lib/index.esm').then(({ default: MonitorSdk }) => {
+            const monitorSdk = MonitorSdk({
+                pid: '1000',
+                reportUrl: 'http://127.0.0.1:9001/report',
+                debug: false,
+                jsErrorLog: true,
+                promiseErrorLog: true,
+                resourcesErrorLog: true,
+                exposureLog: true,
+                automaticBurialPointLog: true,
+                pageDwellTimeLog: true,
+                pvLog: true,
+                xhrLog: true,
+                resourcesPerfLog: true,
+                perfLog: true,
+            });
 
-    monitorSdk.report({
-      type: 'custom',
-      userName: 'angus',
-    });
-  return null;
+            monitorSdk.report({
+                type: 'custom',
+                userName: 'angus',
+            });
+        });
+    }, []);
+
+    return null;
 };
 
 export default MonitorComponent;
--- src/components/common/ClientWeb3Wrapper.tsx
+++ src/components/common/ClientWeb3Wrapper.tsx
@@ -3,10 +3,7 @@ import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
 import { LoadingScreen } from './LoadingScreen';
 
-const Web3Provider = dynamic(() => import('@/components/WagmiConnect/Web3Provider'), {
-  ssr: false,
-  loading: () => <LoadingScreen />,
-});
+const Web3Provider = dynamic(() => import('@/components/WagmiConnect/Web3Provider'));
 
 export function ClientWeb3Wrapper({ children }: { children: ReactNode }) {
   return <Web3Provider>{children}</Web3Provider>;
